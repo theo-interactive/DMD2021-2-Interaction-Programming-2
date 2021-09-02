826605150b0f25db4910eae277c636b0a1434eb2
reunseo <56711584+reunseo@users.noreply.github.com>
2021-09-03 02:51:35 +0900

    1주차 과제

--- 1Week/21961089_leeeunseo/js/script.js
+++ 1Week/21961089_leeeunseo/js/script.js
@@ -1,31 +1,7 @@
-console.log('script load');
+// console.log('js loaded');
 
-// jQuery -> $
-console.log(document);
-console.log(document.documentElement); // <html>
-console.log(document.body); // <body>
-
-// 문서 준비가 완료되었을 때.
-$(document).ready(function() {
-    // window.alert('jQuery 로드가 완료되었습니다.');
-    console.log('jQuery 로드가 완료되었습니다.');
-    console.log('21961089 이은서');
+$(document).ready(function(){
+    alert('21961089 / 이은서');
+    console.log('안녕하세요.\n19학번 이은서입니다.');
+    console.log('오늘 수업 시간에 보여주신 다양한 애니메이션을 이용한 브랜딩 작업물들이 인상깊었습니다.\n이번 학기에도 열심히 배워서 멋진 인터랙션 웹사이트를 만들어보고 싶습니다.\n한 학기 동안 잘 부탁드립니다.');
 });
-
-// $(document).ready(() => {
-
-// });
-
-// 함수.
-// ES 5
-function hello1() {
-    console.log('hello1');
-}
-
-// ES 6 ~
-const hello2 = () => {
-    console.log('hello2');
-}
-
-hello1();
-hello2();
