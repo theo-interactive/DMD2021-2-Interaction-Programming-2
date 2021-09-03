1ec440096a739ff2b2138a351fb791712ae0f74f
21861025 <89962351+KimSuBin21@users.noreply.github.com>
2021-09-03 17:04:23 +0900

    1주차 과제

---
+++ 1Week/21861025_Kimsubin/js/script.js
@@ -0,0 +1,34 @@
+// console.log("script load");
+
+// jQuery -> $
+// console.log(document);
+// console.log(document.documentElement); // <html>
+// console.log(document.body); // <body>
+
+// 문서 준비가 완료되었을 때.
+$(document).ready(function() {
+    // window.alert('jQuery 로드가 완료되었습니다.');
+    // console.log('jQuery 로드가 완료되었습니다.');
+    // console.log('21861025 김수빈');
+    window.alert('21861025 김수빈');
+    console.log("지난 학기에는 이해가 가지않아 목표하는 결과물을 완벽히 수행하지 못했지만 이번 학기에는 원하는 결과물이 진행될수 있도록 열심히 해보겠습니다!! ");
+    console.log("마지막 한학기 잘 부탁드립니다.")
+});
+
+// $(document).ready(() => {
+
+// });
+
+// 함수.
+// ES 5
+// function hello1() {
+//     console.log('hello1');
+// }
+
+// ES 6 ~
+// const hello2 = () => {
+//     console.log('hello2');
+// }
+
+// hello1();
+// hello2();
