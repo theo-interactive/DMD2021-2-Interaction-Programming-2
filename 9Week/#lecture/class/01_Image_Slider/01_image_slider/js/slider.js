417dd88c6f7048a2a191c8271e389b71cef63051
Kim Yong Won <ryonghwon@gmail.com>
2021-10-28 16:27:03 +0900

    9주차 수업 진행
    Image Slider

--- 9Week/#lecture/class/01_Image_Slider/01_image_slider/js/slider.js
+++ 9Week/#lecture/class/01_Image_Slider/01_image_slider/js/slider.js
@@ -1,11 +1,16 @@
 console.log("Script Load");
+
+const IMAGE_WIDTH = 1069;
+
 (function($) {
     $(document).ready(function() {
         console.log('jQuery Ready');
 
+        let isAni = false;
         let cuId = 0;
         let exId = 0;
-        let max = void 0
+        let max = void 0;
+        const _this = this;
 
         const $banner = $('.banner');
         const $wrap = $banner.children('.banner-wrap');
@@ -35,9 +40,33 @@ console.log("Script Load");
             $dot.on('click', handleClickDot)
         }
 
+
+        // arrow function. (event handler)
+        // const handleClickPaddle = (e) => {
+        //     // console.log(this);
+        //     // console.log(e.currentTarget);
+        //     const $el = $(e.currentTarget);
+        //     console.log($el);
+        // }
+
+        // function handleClickPaddle(e) {
+        //     // console.log(_this);
+        //     // console.log(this);
+        //     const $el = $(this);
+        //     // let $el = $(this);
+        //     // console.log($el);
+        //     // $el = $(e.currentTarget);
+        //     // console.log($el);
+        // }
+
+        // function. (event handler)
         function handleClickPaddle(e) {
             e.preventDefault();
-            const $el = $(e.currentTarget);
+            if (isAni) {
+                return;
+            }
+            const $el = $(this);
+            // const $el = $(e.currentTarget);
             if ($el.is($btnPaddlePrev)) {
                 cuId -= 1;
                 if (cuId < 0) {
@@ -49,15 +78,70 @@ console.log("Script Load");
                     cuId = max - 1;
                 }
             }
-            console.log(cuId);
+            if (exId !== cuId) {
+                slideAnimation();
+            }
+        }
+
+        function slideAnimation() {
+            if (!isAni) {
+                isAni = true;
+            }
+            paddleActive();
+            dotSelect();
+            const left = `${(IMAGE_WIDTH * cuId) * -1}px`;
+            const duration = 300 + 100 * Math.abs(cuId - exId);
+            const easing = 'easeInSine'
+            // ease in out
+            // sine / quad / quart / elastic
+            // easeInSine / easeOutSine / easeInOutSine
+            // console.log(exId, cuId, cuId - exId, Math.abs(cuId - exId));
+            // $container.css({ left: `${(IMAGE_WIDTH * cuId) * -1}px` });
+            // exId = cuId;
+
+            // $container.stop(true).animate({ left: left }, 200);
+            // animate({transition 스타일}, {option})
+            // animate({transition 스타일}, number - duration 속도);
+            // $container.stop(true).animate({ left }, 400); // 400ms
+            // $container.stop(true).animate({ left }, { duration: 400, complete: function() {
+            $container.stop(true).animate({ left }, { duration, easing, complete: function() {
+                isAni = false;
+                exId = cuId;
+            }});
         }
 
         function handleClickDot(e) {
             e.preventDefault();
-            console.log('dot');
+            if (isAni) {
+                return;
+            }
+            // const $el = $(this);
+            const idx = $dot.index(this);
+            if (exId !== idx) {
+                cuId = idx
+                slideAnimation();
+            }
+        }
+
+        function paddleActive() {
+            $btnPaddlePrev.removeClass('disabled');
+            $btnPaddleNext.removeClass('disabled');
+            if (cuId === 0) {
+                $btnPaddlePrev.addClass('disabled');
+            } else if (cuId === max - 1) {
+                $btnPaddleNext.addClass('disabled');
+            }
+        }
+
+        function dotSelect() {
+            $dot.removeClass('selected');
+            $dot.eq(cuId).addClass('selected');
         }
 
         function reset() {
+            isAni = false;
+            paddleActive();
+            dotSelect();
         }
 
         init();
