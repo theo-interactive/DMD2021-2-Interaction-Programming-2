05e5834e90d31c8ee9023d6bff20258cae0b6317
Kim Yong Won <ryonghwon@gmail.com>
2021-11-11 15:14:53 +0900

    11주차 수업 진행
    - 스크립트, 스타일 수정.

--- 11Week/#lecture/class/Scroll_Event/js/script.js
+++ 11Week/#lecture/class/Scroll_Event/js/script.js
@@ -2,5 +2,52 @@ console.log("Script Load");
 (function($){
     $(document).ready(function() {
         console.log('jQuery Ready');
+
+        let horizontalMax = 0;
+        // let scrollY = 0;
+
+        const $win = $(window);
+        const $wrap = $('#wrap');
+        const $sticky = $('.section-sticky');
+        const $horizontal = $('.section-horizontal');
+        const $horizontalWrap = $horizontal.children('.section-wrap');
+        const $horizontalItem = $horizontalWrap.children('.section-item');
+        
+        function init() {
+            setting();
+            addEvent();
+            reset();
+        }
+
+        function setting() {
+            horizontalMax = $horizontalItem.length;
+        }
+
+        function addEvent() {
+            $win.on('resize', handleResizeWindow).on('scroll', handleScrollWindow);
+        }
+
+        function handleResizeWindow() {
+            // const width = $win.width();
+            // const height = $win.height();
+            const { width, height } = window
+            $horizontal.outerHeight(horizontalMax * height);
+            $horizontalWrap.outerWidth(horizontalMax * width);
+            $horizontalItem.outerWidth(width);
+        }
+
+        function handleScrollWindow() {
+            // scrollY = $win.scrollTop();
+            const { scrollY } = window
+            console.log(scrollY);
+            
+        }
+
+        function reset() {
+            $win.trigger('resize').trigger('resize');
+            $wrap.removeClass('inactive');
+        }
+
+        init();
     });
 })(jQuery);
