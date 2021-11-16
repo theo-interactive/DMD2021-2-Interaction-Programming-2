f563c0ec0ccf981801a15340653f2ddefc3b457a
juhehyung1 <gudwngml1@kaywon.ac.kr>
2021-11-17 01:23:39 +0900

    11w

---
+++ 11Week/22061125_hyungjuhe/js/script.js
@@ -0,0 +1,130 @@
+console.log("Script Load");
+(function($){
+    $(document).ready(function() {
+        console.log('jQuery Ready');
+
+        let horizontalMax = 0;
+
+        const $win = $(window);
+        const $wrap = $('#wrap');
+        const $section1 = $('#section-1');
+        const $section1Image = $section1.find('img');
+
+        const $section2 = $('#section-2');
+        const $section2Image = $section2.find('img');
+        const $section2Title = $section2.find('.title');
+        const $section2Desc = $section2.find('.desc');
+
+        const $sticky = $('.section-sticky');
+        const $horizontal = $('.section-horizontal');
+        const $horizontalWrap = $horizontal.children('.section-wrap');
+        const $horizontalItem = $horizontalWrap.children('.section-item');
+        
+        function init() {
+            setting();
+            addEvent();
+            reset();
+        }
+
+        function setting() {
+            horizontalMax = $horizontalItem.length;
+        }
+
+        function addEvent() {
+            $win.on('resize', handleResizeWindow).on('scroll', handleScrollWindow);
+        }
+
+        function handleResizeWindow() {
+            const { innerWidth: width, innerHeight: height } = window
+            $horizontal.outerHeight(horizontalMax * height);
+            $horizontalWrap.outerWidth(horizontalMax * width);
+            $horizontalItem.outerWidth(width);
+        }
+
+        function handleScrollWindow() {
+            horizontalScroll();
+        }
+
+        function horizontalScroll() {
+            const { scrollY, innerWidth: width, innerHeight: height } = window
+            let posX = 0;
+            const horizontalWidth = horizontalMax * width
+            const horizontalHeight = horizontalMax * height;
+            const startY = $horizontal.offset().top
+            const endY = startY + horizontalHeight;
+
+            if (scrollY >= startY && scrollY < endY) {
+                if (scrollY < endY - height) {
+                    x = (horizontalWidth - width) * (scrollY - startY) / (horizontalHeight - height) * -1;
+                    $horizontal.addClass('fixed');
+                    $horizontal.removeClass('fixed-end');
+                } else {
+                    $horizontal.removeClass('fixed');
+                    $horizontal.addClass('fixed-end');
+                    x = (horizontalWidth - width) * -1;
+                }
+            } else {
+                $horizontal.removeClass('fixed');
+                if (scrollY < startY) {
+                    x = 0;
+                    $horizontal.removeClass('fixed-end');
+                }
+            }
+            gsap.set($horizontalWrap, { x });
+            
+            const boundY = $horizontal[0].getBoundingClientRect().top
+            if (boundY <= 0 && boundY > $horizontal.outerHeight() * -1) {
+            }
+        }
+
+        function reset() {
+            $win.trigger('resize').trigger('scroll');
+            $wrap.removeClass('inactive');
+            animation();
+            animation2Reset();
+            animation2Start();
+        }
+
+        function animation() {
+            gsap.fromTo($section1Image, 
+                { autoAlpha: 0, scale: 1.05, y: 20, skewX: 5 },
+                { 
+                    autoAlpha: 1, 
+                    scale: 1,
+                    y: 0,
+                    skewX: 0,
+                    duration: 0.6,
+                    delay: 0.2,
+                
+                    ease: 'power3.inOut',
+                    onUpdate: function() {
+                    },
+                    onComplete: function() {
+                        console.log('complete');
+                    }
+                }
+            );
+        }
+
+        function animation2Reset() {
+            gsap.set($section2Image, { y: 20, autoAlpha: 0, scale: 1.05 });
+            gsap.set($section2Title, { y: 40, autoAlpha: 0 });
+            gsap.set($section2Desc, { y: 60, autoAlpha: 0 });
+        }
+
+        function animation2Start() {
+            console.log($section2Image);
+            console.log($section2Title);
+            console.log($section2Desc);
+
+            const tl = gsap.timeline();          
+            tl
+                .add('section2Start')
+                .to($section2Image, { y: 0, autoAlpha: 1, scale: 1, duration: 0.3, delay: 0.5 }, 'section2Start')
+                .to($section2Title, { y: 0, autoAlpha: 1, duration: 1, ease: 'sine.inOut' }, 'section2Start+=0.5')
+                .to($section2Desc, { y: 0, autoAlpha: 1, duration: 1, ease: 'sine.inOut' }, 'section2Start+=0.8')
+        }
+
+        init();
+    });
+})(jQuery);
